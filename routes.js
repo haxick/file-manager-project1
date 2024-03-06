9bd7be42c68c89fb473f09fbc41d449d572c1f3c
haxick <haxick.m1ke@gmail.com>
2024-03-06 22:23:09 +0200

    done!

--- routes.js
+++ routes.js
@@ -38,15 +38,50 @@ router.get("/:filename", (req, res) => {
 router.get("/views/create", (req, res) => {
   res.render("create");
 });
+router.get("/views/update", (req, res) => {
+  res.render("update");
+});
+router.get("/views/delete", (req, res) => {
+  res.render("delete");
+});
 
 router.post("/views/create", (req, res) => {
   const { filename, content } = req.body;
-  const filePath = `${__dirname}/../data/${filename}.txt`;
+  const filePath = `${__dirname}/file-manager-project/../data/${filename}.txt`;
 
   fs.writeFile(filePath, content, (err) => {
     if (err) {
       console.error("Error creating file:", err);
       res.status(500).send("Internal Server Error");
+      return;
+    }
+    res.redirect("/");
+  });
+});
+
+router.post("/views/update", (req, res) => {
+  const { oldFilename, newFilename } = req.body;
+  const oldFilePath = `${__dirname}/file-manager-project/../data/${oldFilename}.txt`;
+  const newFilePath = `${__dirname}/file-manager-project/../data/${newFilename}.txt`;
+
+  fs.rename(oldFilePath, newFilePath, (err) => {
+    if (err) {
+      console.error("Error renaming file:", err);
+      res.status(500).send("Invalid entry");
+      return;
+    }
+    res.redirect("/");
+  });
+});
+
+router.post("/views/delete", (req, res) => {
+  const { filename } = req.body;
+  const filePath = `${__dirname}/file-manager-project/../data/${filename}.txt`;
+
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      console.error("Error deleting file:", err);
+      res.status(500).send("Invalid entry");
       return;
     }
     res.redirect("/");
