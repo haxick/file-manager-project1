80e70b212c82b464c60f7172d60d9db7259ba883
haxick <haxick.m1ke@gmail.com>
2024-03-09 11:20:46 +0200

    last commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const routes = require("./routes");
+const ejs = require("ejs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
